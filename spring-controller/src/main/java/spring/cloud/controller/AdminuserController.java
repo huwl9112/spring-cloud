89db590f062b0616759dfbfc285268e46023fca1
huwl9112 <18112980171@163.com>
2019-04-22 10:31:23 +0800

    给注册中心加上安全认证

--- spring-controller/src/main/java/spring/cloud/controller/AdminuserController.java
+++ spring-controller/src/main/java/spring/cloud/controller/AdminuserController.java
@@ -1,9 +1,13 @@
 package spring.cloud.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import spring.cloud.iservice.UserService;
+
+import javax.annotation.PostConstruct;
 
 /**
  * <p>
@@ -15,7 +19,24 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 @RequestMapping("/adminuser")
-public class AdminuserController {
+public class AdminuserController implements InitializingBean {
+    @Autowired
+    private UserService userService;
+    private String name="huel";
+
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("=========2");
+        this.name="huwl";
+
+    }
+
+    @PostConstruct
+    public void setName(){
+        System.out.println("=========1");
+        this.name="wdm";
+    }
 
 }
 
---
+++ spring-register/src/main/java/spring/cloud/register/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package spring.cloud.register;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Date: 2019/4/22 9:33
+ * @Author: huwl
+ * @Description:
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
